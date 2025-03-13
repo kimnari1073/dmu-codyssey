6484ee33e4bbff32922e435336acc2a9bdb368cd
kimnari1073 <107450730+kimnari1073@users.noreply.github.com>
2025-03-13 20:57:06 +0900

    fix: 피드백 반영 및 보너스 문제 추가

--- mission01/main.py
+++ mission01/main.py
@@ -1,32 +1,38 @@
 # main.py mission 01
 print('Hello Mars')
 
+problem_lines=[]
 output_file = 'problematic_logs.txt'
+
 try:
     with open('mission_computer_main.log', 'r', encoding='utf-8') as log_file:
-        log_contents = log_file.read()
-    # 문제되는 부분만 필터링 (예: "Oxygen tank unstable" 또는 "Oxygen tank explosion" 같은 줄)
-    problem_lines = [line for line in log_contents if 'Oxygen tank' in line]
+        log_contents = log_file.readlines()
+    
+    # mission_computer_main.log 파일 출력 & "Oxygen tank" 단어 포함 시 배열 저장
+    for line in log_contents:
+        print(line.strip()) 
+        if 'Oxygen tank' in line:
+            problem_lines.append(line)
 
-    # 문제되는 부분을 별도의 파일에 저장.
+    # "Oxygen tank"단어가 포함된 문장을 별도의 파일에 저장.
     if problem_lines:
         with open(output_file, 'w', encoding='utf-8') as output:
             output.writelines(problem_lines)
         print(f"문제가 되는 부분을 '{output_file}'에 저장했습니다.")
+        print("문제가 되는 부분은 다음과 같습니다:")
+        for line in problem_lines:
+            print(line)
     else:
         print("문제가 되는 로그 항목이 없습니다.")
-except FileNotFoundError:
+except FileNotFoundError :
     print('Error: mission_computer_main.log 파일을 찾을 수 없습니다.')
 except Exception as e:
     print(f'Error: 알 수 없는 오류가 발생했습니다. ({e})')
     
-print(log_contents)
-
-#출력 결과를 시간의 역순으로 정렬해서 출력한다. 
+#역순 출력
 with open('mission_computer_main.log','r',encoding='utf-8') as log_file:
     lines = log_file.readlines()  
 
+print("출력 결과를 시간의 역순으로 정렬해서 출력한다.")
 for line in reversed(lines):
     print(line.strip())  
-
-#출력 결과 중 문제가 되는 부분만 따로 파일로 저장한다. 
